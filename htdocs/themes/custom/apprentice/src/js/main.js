dd5b1bce67cbc0572ee5d279dbc350d5fb3379d9
NGroeneboom <nataliegroeneboom@yahoo.co.uk>
2018-11-26 11:53:12 +0000

    add htdoc files

---
+++ htdocs/themes/custom/apprentice/src/js/main.js
@@ -0,0 +1,55 @@
+(function($){
+
+	Drupal.behaviors.apprentice = {
+		attach:function(context, settings){
+
+
+
+
+
+			$(".topics-covered.zebra-odd").addClass("col-md-4");
+			$(".topics-covered.zebra-even").addClass("col-md-4");
+			$("#block-apprentice-main-menu").addClass("navbar");
+
+
+
+
+// function navBar(){
+// 	var collapse = document.getElementById("block-apprentice-main-menu");
+//
+// 	 if(collapse.className === "navbar"){
+// 		 	collapse.className += " responsive";
+// 	 }else{
+// 		 collapse.className === "navbar";
+// 	 }
+//
+// }
+//
+//
+	$(".navbar > .main-menu").once().append('<li class="icon"><a href="javascript:void(0);" ><i class="fa fa-bars"></i></a></li>');
+
+
+
+$('.icon').once().click(function(){
+	$('.main-menu').toggleClass('responsive');
+	$('.header').toggleClass('responsive');
+    $('.layout-container').toggleClass('responsive');
+
+});
+
+
+
+
+
+
+
+
+
+} //end of drupal jquery
+
+
+
+	}
+
+
+})(jQuery);
